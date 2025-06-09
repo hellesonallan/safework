892c012f712d29c3b6b90576a3d3909a6452ee1b
Helleson Allan <113473921+hellesonallan@users.noreply.github.com>
2025-06-08 22:46:20 -0300

    feat: tela de perfil finalizada

--- app/profile.tsx
+++ app/profile.tsx
@@ -6,6 +6,14 @@ import { Input } from "~/components/ui/input";
 import { useState } from "react";
 import { Avatar, AvatarImage, AvatarFallback } from "~/components/ui/avatar";
 import { Text } from "~/components/ui/text";
+import { Button } from "~/components/ui/button";
+import { Separator } from "~/components/ui/separator";
+import { Bell } from "~/lib/icons/Bell";
+import { Contrast } from "~/lib/icons/Contrast";
+import { ScrollText } from "~/lib/icons/ScrollText";
+import { LogOut } from "~/lib/icons/LogOut";
+import { Info } from "~/lib/icons/Info";
+import { MessageSquareWarning } from "~/lib/icons/MessageSquareWarning";
 
 export default function Screen() {
   const [value, setValue] = useState("");
@@ -21,7 +29,7 @@ export default function Screen() {
         title="Meu Perfil"
         subtitle="Visualize seu perfil e suas configurações"
       />
-      <View className="flex-row items-center">
+      <View className="flex-row items-center mb-4">
         <Avatar className="w-24 h-24 mr-4" alt="Avatar">
           <AvatarImage source={require("~/assets/images/avatar.png")} />
           <AvatarFallback>
@@ -33,6 +41,63 @@ export default function Screen() {
           <Text className="font-light">joao.silva@mail.com</Text>
           <Text className="font-light">Funcionário - Setor Administrativo</Text>
         </View>
+      </View>
+      <View>
+        <Button
+          className="flex-row gap-2 mb-4 justify-start"
+          variant={"outline"}
+        >
+          <Bell className="text-forebackground" size={24} strokeWidth={2} />
+          <Text>Notificações</Text>
+        </Button>
+        <Separator className="mb-4" />
+        <Button
+          className="flex-row gap-2 mb-4 justify-start"
+          variant={"outline"}
+        >
+          <Contrast className="text-forebackground" size={24} strokeWidth={2} />
+          <Text>Modo Alto Contraste</Text>
+        </Button>
+        <Separator className="mb-4" />
+        <Button
+          className="flex-row gap-2 mb-4 justify-start"
+          variant={"outline"}
+        >
+          <Info className="text-forebackground" size={24} strokeWidth={2} />
+          <Text>Suporte Online</Text>
+        </Button>
+        <Separator className="mb-4" />
+        <Button
+          className="flex-row gap-2 mb-4 justify-start"
+          variant={"outline"}
+        >
+          <MessageSquareWarning
+            className="text-forebackground"
+            size={24}
+            strokeWidth={2}
+          />
+          <Text>Sobre o SafeWork</Text>
+        </Button>
+        <Separator className="mb-4" />
+        <Button
+          className="flex-row gap-2 mb-4 justify-start"
+          variant={"outline"}
+        >
+          <ScrollText
+            className="text-forebackground"
+            size={24}
+            strokeWidth={2}
+          />
+          <Text>Termos de Uso</Text>
+        </Button>
+        <Separator className="mb-4" />
+        <Button
+          className="flex-row gap-2 mb-4 justify-start"
+          variant={"outline"}
+        >
+          <LogOut className="text-forebackground" size={24} strokeWidth={2} />
+          <Text>Sair</Text>
+        </Button>
       </View>
     </ScrollView>
   );
---
+++ lib/icons/Contrast.tsx
@@ -0,0 +1,4 @@
+import { Contrast } from "lucide-react-native";
+import { iconWithClassName } from "./iconWithClassName";
+iconWithClassName(Contrast);
+export { Contrast };
